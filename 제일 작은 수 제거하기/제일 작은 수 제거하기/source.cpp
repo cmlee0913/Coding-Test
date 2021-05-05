734bf7c8221ed5d80435002ccca871e6059e2733
Lee chang min <cmlee0913@naver.com>
2021-05-06 01:24:45 +0900

    05.06

--- 제일 작은 수 제거하기/제일 작은 수 제거하기/source.cpp
+++ 제일 작은 수 제거하기/제일 작은 수 제거하기/source.cpp
@@ -5,21 +5,24 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) {
     vector<int> answer;
-    int min;
+    int min = 1000;
 
-    for (int i = 1; i < arr.size(); i++) {
-        if (arr[i] > arr[i - 1]) {
-            min = arr[i - 1];
+    if (arr.size() > 1) {
+        for (int i = 0; i < arr.size(); i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
         }
-        else if (arr[i] < arr[i - 1]) {
-            min = arr[i];
-        }
-        else {
-            min = arr[i];
+        for (int j = 0; j < arr.size(); j++) {
+            if (arr[j] != min) {
+                answer.push_back(arr[j]);
+            }
         }
     }
 
-
+    else if (arr.size() == 1) {
+        answer.push_back(-1);
+    }
 
     return answer;
 }
