ae83d3972c3069fff73de0df3768b67795f436ba
Lee chang min <cmlee0913@naver.com>
2021-05-07 17:06:11 +0900

    05.07

---
+++ 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기/source.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    string s;
+
+    s = to_string(n);
+
+    reverse(begin(s), end(s));
+
+    for (int i = 0; i < s.size(); i++) {
+        answer.push_back(s[i] - '0');
+    }
+
+    return answer;
+}
