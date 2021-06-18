bda32c1015a206029bcf5de4d43e32b5719b71da
Lee chang min <cmlee0913@naver.com>
2021-06-18 23:55:24 +0900

    06.18

--- 2016년 (배열, 날짜 계산)/2016년 (배열, 날짜 계산)/소스.cpp
+++ 2016년 (배열, 날짜 계산)/2016년 (배열, 날짜 계산)/소스.cpp
@@ -1,7 +1,7 @@
 #include <string>
 #include <vector>
-
 using namespace std;
+
 string str_day[] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
 int month_day[] = { 31,29,31,30,31,30,31,31,30,31,30,31 };
 
@@ -9,12 +9,12 @@ string solution(int a, int b) {
     int total_day = 0;
     
     // 달
-    for(int i=1; i<a; i++)
+    for(int i = 1; i < a; i++)
     {
         total_day += month_day[i-1];
     }
     // 일
-    total_day += b-1;
+    total_day += b - 1;
  
     // 2016.1.1 = 금
     int answer_day = (5 + total_day) % 7;
