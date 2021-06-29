a5fc9d479e5cd94e91cf90ac40000cf4a07c69a5
leecmin <cmlee0913@naver.com>
2021-06-29 20:30:19 +0900

    06.29

--- 체육복 (그리디)/체육복 (그리디)/source.cpp
+++ 체육복 (그리디)/체육복 (그리디)/source.cpp
@@ -20,3 +20,40 @@ int solution(int n, vector<int> lost, vector<int> reserve) {
 
     return answer;
 }
+
+// 다른 풀이
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = n - lost.size();
+
+    for (int i = 0; i < lost.size(); i++) {
+        for (int j = 0; j < reserve.size(); j++) {
+            if (reserve[j] == lost[i]) {
+                lost.erase(lost.begin() + (i--));
+                reserve.erase(reserve.begin() + (j--));
+                answer++;
+                break;
+            }
+            else if (reserve[j] - 1 == lost[i]) {
+                lost.erase(lost.begin() + (i--));
+                reserve.erase(reserve.begin() + (j--));
+                answer++;
+                break;
+            }
+            else if (reserve[j] + 1 == lost[i]) {
+                lost.erase(lost.begin() + (i--));
+                reserve.erase(reserve.begin() + (j--));
+                answer++;
+                break;
+            }
+
+        }
+    }
+
+
+    return answer;
+}
