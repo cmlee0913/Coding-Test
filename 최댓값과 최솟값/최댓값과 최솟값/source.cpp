81c3d1a44aa4ced539fef9c6ed715e1a1fc151bd
leecmin <cmlee0913@naver.com>
2021-07-08 23:22:13 +0900

    07.08

--- 최댓값과 최솟값/최댓값과 최솟값/source.cpp
+++ 최댓값과 최솟값/최댓값과 최솟값/source.cpp
@@ -1,47 +1,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
 string solution(string s) {
     string answer = "";
+    string token = "";
     vector<int> v;
 
     for (int i = 0; i < s.size(); i++) {
-        string token = "";
-        if (i == 0) {
-            if (s[i] == '-') {
-                token += s[i];
-                token += s[i + 1];
-            }
-            else {
-                token += s[i];
-            }
-        }
         if (s[i] == ' ') {
-            if (i > 0) {
-                if (s[i + 1] == '-') {
-                    token += s[i + 1];
-                    token += s[i + 2];
-                }
-                else {
-                    token += s[i + 1];
-                }
-            }
+            v.push_back(atoi(token.c_str()));
+            token.clear();
         }
-        v.push_back(stoi(token));
+
+        else {
+            token += s[i];
+        }
     }
+    v.push_back(atoi(token.c_str()));
     sort(v.begin(), v.end());
 
-    answer += to_string(v[0]);
-    answer += to_string(v[v.size() - 1]);
+    answer += to_string(v[0]) + " " + to_string(v[v.size() - 1]);
 
     return answer;
-}
-
-int main() {
-    cout << solution("1 2 3 4");
-
-    return 0;
 }
