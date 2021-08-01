8f22a50d7c424d49aea6d68c72cb483e3be4151f
Lee chang min <cmlee0913@naver.com>
2021-08-01 23:37:05 +0900

    08.01

--- K번째 수 (sort)/K번째 수 (sort)/source.cpp
+++ K번째 수 (sort)/K번째 수 (sort)/source.cpp
@@ -8,9 +8,8 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
 
     for (int i = 0; i < commands.size(); i++) {
         vector<int> s;
-        for (int j = commands[i][0]; j <= commands[i][1]; j++) {
+        for (int j = commands[i][0]; j <= commands[i][1]; j++) 
             s.push_back(array[j - 1]);
-        }
         sort(s.begin(), s.end());
         answer.push_back(s[commands[i][2] - 1]);
     }
