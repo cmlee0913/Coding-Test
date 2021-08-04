b5998205706939e0fb827f0244cf21065e0a9f77
leecmin <cmlee0913@naver.com>
2021-08-04 23:31:05 +0900

    08.04

--- 소수 찾기 (순열, 완전 탐색, set)/소수 찾기 (순열, 완전 탐색, set)/source.cpp
+++ 소수 찾기 (순열, 완전 탐색, set)/소수 찾기 (순열, 완전 탐색, set)/source.cpp
@@ -15,15 +15,15 @@ bool sosu(int num) {
 
 int solution(string numbers) {
     unordered_set<int> a;
-    int b = 0;
+    int temp = 0;
 
     sort(numbers.begin(), numbers.end());
 
     do {
         for (int i = 1; i <= numbers.size(); i++) {
-            b = stoi(numbers.substr(0, i));
-            if (sosu(b))
-                a.insert(b);
+            temp = stoi(numbers.substr(0, i));
+            if (sosu(temp))
+                a.insert(temp);
         }
 
     } while (next_permutation(numbers.begin(), numbers.end()));
