7e88503d664d960789ff49d9459b562ccf8f6fc9
leecmin <cmlee0913@naver.com>
2021-08-13 17:59:15 +0900

    08.13

--- 가장 큰 수(정렬)/가장 큰 수(정렬)/source.cpp
+++ 가장 큰 수(정렬)/가장 큰 수(정렬)/source.cpp
@@ -14,9 +14,8 @@ string solution(vector<int> numbers) {
     string answer = "";
     vector<string> num_to_str;
 
-    for (int i = 0; i < numbers.size(); i++) {
+    for (int i = 0; i < numbers.size(); i++) 
         num_to_str.push_back(to_string(numbers[i]));
-    }
 
     sort(num_to_str.begin(), num_to_str.end(), bigger);
     if (num_to_str[0] == "0") return "0";
