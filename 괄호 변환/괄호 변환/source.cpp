c135c6c4984bfe146444875857ba093d2176c37e
leecmin <cmlee0913@naver.com>
2021-08-15 01:32:23 +0900

    08.15

--- 괄호 변환/괄호 변환/source.cpp
+++ 괄호 변환/괄호 변환/source.cpp
@@ -1,20 +1,73 @@
 #include <string>
 #include <vector>
-
+#include <iostream>
 using namespace std;
 
-string solution(string p) {
-    string answer = "";
+bool isBalance(string s) {
     int balance = 0;
 
-    for (auto Bracket : p) {
-        if (Bracket == '(') {
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
             balance--;
         }
         else {
             balance++;
         }
+        if (balance == 0) {
+            return true;
+        }
+        else if (balance > 0) {
+            return false;
+        }
+    }
+}
+
+string balanceBracket(string s) {
+    if (s == "") return s;
+    int balance = 0;
+    string u;
+    string v;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            balance--;
+        }
+        else {
+            balance++;
+        }
+        if (balance == 0) {
+            u = s.substr(0, i + 1);
+            v = s.substr(i + 1);
+            break;
+        }
     }
 
-    return answer;
+    if (isBalance(u)) {
+        return u + balanceBracket(v);
+    }
+
+    else if (!isBalance(u)) {
+        string str;
+        str = "(" + balanceBracket(v) + ")";
+        u = u.substr(1, u.length() - 2);
+        for (int i = 0; i < u.size(); i++) {
+            if (u[i] == '(') {
+                str += ')';
+            }
+            else {
+                str += '(';
+            }
+        }
+        return str;
+    }
+}
+
+string solution(string p) {
+    return balanceBracket(p);
+}
+
+int main() {
+    cout << solution("()))((()");
+
+    return 0;
 }
