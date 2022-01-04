255b8154f0bbd1c1dbea20d4bf6822177066f21f
Lee chang min <cmlee0913@naver.com>
2022-01-04 20:20:19 +0900

    01.04

---
+++ 연습.cs
@@ -0,0 +1,67 @@
+// 추상, 가상, 인터페이스 연습
+/* public abstract class Super {
+    public abstract void attack();
+    public abstract void attack(int a);
+
+    public virtual void stop() {
+
+    }
+    public virtual void stop(int a) {
+
+    }
+}
+
+public interface ifa {
+    void isCheck(int a);
+    int asd {
+        get;
+        set;
+    }
+}
+
+public class child : Super, ifa {
+    private int qwe;
+    public int asd { get {return qwe;} set {qwe = value;} }
+
+    public override void attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void attack(int a)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override bool Equals(object obj)
+    {
+        return base.Equals(obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public void isCheck(int a)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void stop() {
+        
+    }
+
+    public override void stop(int a) {
+        
+    }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
+
+public class class2 : child {
+    child a = new child();
+} */
