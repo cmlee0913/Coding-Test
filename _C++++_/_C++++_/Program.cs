150eb7e94440db2626cc55f16acb9ef830ff3ae6
Lee chang min <cmlee0913@naver.com>
2022-04-27 23:14:13 +0900

    04.27

--- _C++++_/_C++++_/Program.cs
+++ _C++++_/_C++++_/Program.cs
@@ -66,6 +66,7 @@ namespace _C_____
 
             // 연산자를 이용하여 델리게이트 내부에 더하거나 빼줄수 있음.
             del += Plus;
+            del += delegate { Console.WriteLine("무명 메서드를 활용 : {0}", 777); return 15; };
 
             // WriteLine은 가장 끝의 인덱스에 있는 리턴 정보를 가져온다. 즉, Minus의 반환값이 아닌 Plus의 반환값만 가져온다.
             Console.WriteLine("가장 마지막 인덱스의 반환값입니다 : {0}", del(5, 5, 5));
@@ -75,20 +76,67 @@ namespace _C_____
         // 메서드의 파라미터로도 사용할 수 있다.
         public void DelExample(DelEx ex)
         {
-            // WriteLine은 가장 끝의 인덱스에 있는 리턴 정보를 가져온다. 즉, Minus의 반환값이 아닌 Plus의 반환값만 가져온다.
-            Console.WriteLine("가장 마지막 인덱스의 반환값입니다 : {0}", ex(5, 5, 5));
-            Console.WriteLine(new string { "ㅎㅇㅎㅇ" }, new string { "ㅂㅇㅂㅇ" });
-            // 스트링 목록을 넣었을땐 맨 처음만 송출한다.
-            Console.WriteLine("ㅎㅇㅎㅇ", "ㅂㅇㅂㅇ");
+            // WriteLine은 가장 마지막 메서드에 있는 리턴 정보를 가져온다. 즉, Minus의 반환값이 아닌 Plus의 반환값만 가져온다.
+            Console.WriteLine("가장 마지막 메서드의 반환값입니다 : {0}", ex(5, 5, 5));
         }
 
+    }
+
+    public class EventEx
+    {
+        public delegate int ExDel(params int[] a);
+        // event는 delegate와 보안 수준을 맞춰주어야 한다.
+        public event ExDel exEvent;
+
+
+        // 이벤트에 넣어줄 함수 1.
+        public int Plus(params int[] a)
+        {
+            int sum = 0;
+            foreach (var i in a) { sum += i; };
+            Console.WriteLine("Plus에 들렀습니다 : {0}", sum);
+            return sum;
+        }
+
+        // 이벤트에 넣어줄 함수 2.
+        public int Minus(params int[] a)
+        {
+            int sum = 0;
+            foreach (var i in a) { sum -= i; };
+            Console.WriteLine("Minus에 들렀습니다 : {0}", sum);
+            return sum;
+        }
+
+        public void UseEvent() {
+            // 클래스 내부에서는 호출 및 할당 둘 다 가능.
+            ExCallBack(exEvent);
+            exEvent = Plus;
+        }
+
+        // 델리게이트형 파라미터로도 할용 가능.
+        void ExCallBack(ExDel exDel) {
+            exDel(5, 5, 5, 5, 5);
+        }
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            DelegateEx.instance.DelChain();
+            //DelegateEx.instance.DelChain();
+
+            EventEx eventEx = new EventEx();
+
+            // 증감 연산자를 이용한 접근만 가능.
+            eventEx.exEvent += eventEx.Plus;
+            eventEx.exEvent += eventEx.Minus;
+
+            // 클래스 외부에서 호출 및 할당을 할 수 없음.
+            // eventEx.exEvent = eventEx.Plus;
+            // eventEx.exEvent();
+
+            eventEx.UseEvent();
+
         }
     }
 }
